aa8308086d39cfcef3cf40130cc2324bdea293e9
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-09-10 16:52:26 +0200

    Added google font to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/global.css";
+import type { AppProps } from "next/app";
+import { Be_Vietnam_Pro } from "next/font/google";
 
-interface IApp {
-	Component: any;
-	pageProps: any;
-}
+const beVietnamPro = Be_Vietnam_Pro({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	style: ["normal", "italic"],
+	subsets: ["latin"],
+	display: "swap",
+});
 
-export default function App({ Component, pageProps }: IApp) {
-	return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<main className={beVietnamPro.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
