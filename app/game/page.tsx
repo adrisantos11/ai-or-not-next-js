61bf0154fafc43c48656015b9ae0784a52870dc1
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-10-01 17:36:30 +0200

    Added responsive to home page and game page.

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -13,93 +13,107 @@ import Link from "next/link";
 const GamePage = () => {
 	const [selected, setSelected] = React.useState<number>(0);
 
-	fetch(`${process.env.NEXT_PUBLIC_API}/api/test`)
-		.then((data) => {
-			console.log(data);
-			return data.json();
-		})
-		.then((data) => console.log(data));
+	React.useEffect(() => {
+		fetch(`${process.env.NEXT_PUBLIC_API}/api/test`)
+			.then((data) => {
+				console.log(data);
+				return data.json();
+			})
+			.then((data) => console.log(data));
+	}, []);
 
 	return (
-		<div className="p-game-page">
+		<div className="p-game">
 			<Link href="/">
 				<Image
 					src={backgIcon.src}
 					alt="back-icon"
+					className="p-game__icon-back"
 					width={50}
 					height={50}
-					className="p-game-page__icon-back"
 				></Image>
 			</Link>
 			<div
-				className="p-game-page__image p-game-page__image--left"
+				className="p-game__image p-game__image--left"
 				style={{ backgroundImage: `url(${bckgImg1.src})` }}
 				onClick={() => {
 					setSelected(1);
-					console.log(true);
 				}}
 			>
 				{selected === 0 ? (
-					<div className="p-game-page__select-img">
-						<span className="p-game-page__select-img-txt">
+					<div className="p-game__select-img">
+						<span className="p-game__select-img-txt">
 							Select image
 						</span>
 					</div>
 				) : (
 					""
 				)}
-				<div className="p-game-page__img-data">
-					<span className="p-game-page__image-title">Image_name</span>
-					<span className="p-game-page__image-description">
-						Description
-					</span>
-				</div>
+				{selected === 0 ? (
+					<div className="p-game__img-data">
+						<span className="p-game__image-title">Image_name</span>
+						<span className="p-game__image-description">
+							Description
+						</span>
+					</div>
+				) : (
+					""
+				)}
 				{selected === 1 ? (
-					<div className="p-game-page__result">
-						<Image
-							src={successImg.src}
-							alt="Success"
-							width={225}
-							height={225}
-						></Image>
-						<span className="p-game-page__result-txt">SUCCESS</span>
+					<div className="p-game__result">
+						<div className="p-game__result-img">
+							<Image
+								src={successImg.src}
+								alt="Success"
+								fill
+							></Image>
+						</div>
+						<span className="p-game__result-txt">SUCCESS</span>
 					</div>
 				) : (
 					""
 				)}
 			</div>
 			<div
-				className="p-game-page__image p-game-page__image--right"
+				className="p-game__image p-game__image--right"
 				style={{ backgroundImage: `url(${bckgImg2.src})` }}
-				onClick={() => {
-					setSelected(2);
-					console.log(false);
-				}}
+				{...(selected === 0 && {
+					onClick: () => {
+						setSelected(2);
+					},
+				})}
 			>
 				{selected === 0 ? (
-					<div className="p-game-page__select-img">
-						<span className="p-game-page__select-img-txt">
+					<div className="p-game__select-img">
+						<span className="p-game__select-img-txt">
 							Select image
 						</span>
 					</div>
 				) : (
 					""
 				)}
-				<div className="p-game-page__img-data">
-					<span className="p-game-page__image-title">Image_name</span>
-					<span className="p-game-page__image-description">
-						Description
-					</span>
-				</div>
+				{selected === 0 ? (
+					<div className="p-game__img-data">
+						<span className="p-game__image-title">Image_name</span>
+						<span className="p-game__image-description">
+							Description
+						</span>
+					</div>
+				) : (
+					""
+				)}
 				{selected === 2 ? (
-					<div className="p-game-page__result">
-						<Image
-							src={wrongImg.src}
-							alt="Wrong"
-							width={225}
-							height={225}
-						></Image>
-						<span className="p-game-page__result-txt">WRONG</span>
+					<div className="p-game__result">
+						<div className="p-game__result-img">
+							<Image
+								src={wrongImg.src}
+								alt="Wrong"
+								className="p-game__result-img"
+								fill
+							></Image>
+						</div>
+
+						<span className="p-game__result-txt">WRONG</span>
 					</div>
 				) : (
 					""
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import styles from "./page.module.css";
 import Link from "next/link";
 import Button from "@mui/material/Button";
 import bckgImg from "@/public/landscape20.jpeg";
 import { TextField } from "@mui/material";
+import "./page.scss";
 
 export default function Home() {
 	return (
 		<main
-			className={styles.main}
+			className="p-home"
 			style={{
 				backgroundImage: `url(${bckgImg.src})`,
 			}}
 		>
-			<h1 className={styles.title}>Is this AI or not</h1>
-			<div className={styles.subtitle}>
+			<h1 className="p-home__title">Is this AI or not</h1>
+			<div className="p-home__subtitle">
 				<h3>Show your capacity of AI detecting</h3>
 				<h3>
 					Learn the main differences between real-world images and AI
@@ -25,60 +25,27 @@ export default function Home() {
 				id="playerName"
 				label="Player name"
 				variant="standard"
-				sx={{ fontFamily: "inherit", width: "30rem" }}
-				InputLabelProps={{
-					sx: {
-						color: "#8B8B8B",
-						fontFamily: "inherit",
-						fontSize: "1.25rem",
-						lineHeight: "1.5rem",
-					},
-				}}
-				InputProps={{
-					sx: {
-						color: "white",
-						textAlign: "center",
-						borderBottom: "2px solid white",
-						"::placeholder": {
-							color: "white",
-						},
-						fontFamily: "inherit",
-						padding: ".125rem 0",
-					},
-				}}
+				className="p-home__user-input"
 			/>
-			<div className={styles["buttons-container"]}>
-				<div className={styles["game-buttons"]}>
-					<Button
-						variant="contained"
-						sx={{
-							borderRadius: "1.5rem",
-							textTransform: "none",
-							fontFamily: "inherit",
-							fontSize: "1rem",
-							padding: ".5rem 2rem",
-							backgroundColor: "#4477CE",
-						}}
-					>
-						<Link href="/game">Start game</Link>
-					</Button>
-					<Button
-						variant="contained"
-						// className={styles["dashboard-button"]}
-						sx={{
-							borderRadius: "1.5rem",
-							textTransform: "none",
-							fontFamily: "inherit",
-							fontSize: "1rem",
-							padding: ".5rem 2rem",
-							backgroundColor: "#512B81",
-							":hover": {
-								backgroundColor: "#37185f",
-							},
-						}}
-					>
-						<Link href="/dashboard">Dashboard</Link>
-					</Button>
+			<div className="p-home__buttons-container">
+				<div className="p-home__game-buttons">
+					<Link href="/game">
+						<Button
+							className="p-home__button p-home__button--game"
+							variant="contained"
+						>
+							Start game
+						</Button>
+					</Link>
+
+					<Link href="/dashboard">
+						<Button
+							variant="contained"
+							className="p-home__button p-home__button--dashboard"
+						>
+							Dashboard
+						</Button>
+					</Link>
 				</div>
 				<Button
 					variant="outlined"
