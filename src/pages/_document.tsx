e7671db445e6a9e09e5ad9d2e688a3637f14c097
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-11-19 17:57:48 +0100

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="shortcut icon" href="src/assets/favicon.ico" />
+				<link rel="shortcut icon" href="/static/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
